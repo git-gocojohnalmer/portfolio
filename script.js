2627100ad6ef181ed627854e7182f431cc6b2878
git-gocojohnalmer <almergoco@gmail.com>
2025-04-13 12:56:44 +0800

    Update

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu(){
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 } 
+
+function toggleSubmenu(event) {
+    const submenu = event.currentTarget.nextElementSibling;
+    const icon = event.currentTarget.querySelector('.submenu-icon');
+    submenu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
+const menuLinks = document.querySelectorAll('.menu-link');
+const submenuLinks = document.querySelectorAll('.submenu-link');
